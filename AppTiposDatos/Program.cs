cdf6880b46d27269c8e9e1729371e3bf854a82a6
HP <HP@JESICA-SORIA>
2025-04-23 02:50:26 -0500

    Agregar archivos de proyecto.

---
+++ AppTiposDatos/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTiposDatos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int entero = 10;
+             double flotante = 20.5;
+             bool booleano = true;
+             char caracter = 'C';
+             string texto = "Ejemplo";
+             Console.WriteLine($"Entero: {entero}");
+             Console.WriteLine($"Flotante: {flotante}");
+             Console.WriteLine($"Booleano: {booleano}");
+             Console.WriteLine($"Caracter: {caracter}");
+             Console.WriteLine($"Texto: {texto}");
+             Console.ReadKey();*/
+
+
+            /*  double valorDouble = 12.78;
+              int valorEntero = (int)valorDouble;
+              string numeroTexto = "50";
+              int numeroEntero = Convert.ToInt32(numeroTexto);
+              Console.WriteLine($"Valor entero (casting): {valorEntero}");
+              Console.WriteLine($"Número convertido: {numeroEntero}");
+              Console.ReadKey();*/
+
+            /* int edad = 22;
+             if (edad >= 18)
+                 Console.WriteLine("Es adulto");
+             else
+                 Console.WriteLine("Es menor");
+             Console.ReadKey();*/
+
+
+            /*int opcion = 2;
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("Opción 1");
+                   break;
+                case 2:
+                    Console.WriteLine("Opción 2");
+
+                    break;
+                default:
+                    Console.WriteLine("Otra opción");
+                    break;
+            }
+            Console.ReadKey();*/
+
+
+            /*for (int i = 0; i < 3; i++)
+                Console.WriteLine($"Iteración {i}");
+            Console.ReadKey();*/
+
+
+            /*int contador = 0;
+            while (contador < 3)
+            {
+                Console.WriteLine($"Contador: {contador}");
+                contador++;
+            }
+            Console.ReadKey();*/
+
+
+            /*int contador = 0;
+            do
+            {
+                Console.WriteLine($"Contador: {contador}");
+
+                contador++;
+            } while (contador < 3);
+            Console.ReadKey();*/
+
+        }
+    }
+
+}
+
+
